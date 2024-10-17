b21fa2ab95fde65da12f464381fb104da860a733
agida0413 <144654789+agida0413@users.noreply.github.com>
2024-10-17 23:57:11 +0900

    sds

--- backend/src/test/java/com/yj/BackendApplicationIT.java
+++ backend/src/test/java/com/yj/BackendApplicationIT.java
@@ -33,7 +33,7 @@ class BackendApplicationIT {
 		
 		repository.join(dto);
 		
-		MemberDTO findDto = repository.findByUsername("agida4132");
+		MemberDTO findDto = repository.findByUsername("");
 		
 		assertThat(findDto.getUsername()).isEqualTo("agida4132");
 	}
