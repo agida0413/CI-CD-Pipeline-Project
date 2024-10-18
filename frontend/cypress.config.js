5e426578310cd5fffaee1980d12dca5313e994ed
KimYJInComapny <agida0413>
2024-10-18 16:48:29 +0900

    'sdasd'

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000', // Docker Compose의 nginx 서비스 이름
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
